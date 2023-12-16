e587f58c826bd8ce22d8145e6a578d8be4f0f129
guest271314 <guest271314@gmail.com>
2023-12-16 06:11:32 -0800

    Create rollup.wbn.js

---
+++ rollup.wbn.js
@@ -0,0 +1,45 @@
+import wbnOutputPlugin from "./lib/index.js";
+import * as wbnSign from "wbn-sign";
+import * as rollup from "rollup";
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+const privateKeyFile = "ed25519key.pem";
+const privateKey = fs.readFileSync(privateKeyFile);
+
+const build = async () => {
+  const key = wbnSign.parsePemKey(
+    privateKey
+  );
+
+  const bundle = await rollup.rollup({
+    input: "./src/script.js",
+    output: { dir: "dist", format: "esm" },
+    plugins: [
+      wbnOutputPlugin({
+        baseURL: new wbnSign.WebBundleId(
+          key,
+        ).serializeWithIsolatedWebAppOrigin(),
+        static: { dir: "assets" },
+        output: "signed.swbn",
+        integrityBlockSign: {
+          strategy: new wbnSign.NodeCryptoSigningStrategy(key),
+        },
+        headerOverride: {
+          "cross-origin-embedder-policy": "require-corp",
+          "cross-origin-opener-policy": "same-origin",
+          "cross-origin-resource-policy": "same-origin",
+          "content-security-policy":
+            "base-uri 'none'; default-src 'self'; object-src 'none'; frame-src 'self' https: blob: data:; connect-src 'self' https: wss:; script-src 'self' 'wasm-unsafe-eval'; img-src 'self' https: blob: data:; media-src 'self' https: blob: data:; font-src 'self' blob: data:; style-src 'self' 'unsafe-inline'; require-trusted-types-for 'script';",
+        },
+      }),
+    ],
+  });
+
+  const { output } = await bundle.generate({ format: 'esm' });
+  const [{ fileName, source }] = output;
+  fs.writeFileSync(fileName, source);
+};
+
+build()
+.then(console.log).catch(console.error);
