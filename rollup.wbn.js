ed5847ebdbe29b0070485f3c1d296022ac4c7e44
guest271314 <guest271314@gmail.com>
2023-12-25 18:05:41 -0800

    Update rollup.wbn.js
    Include more wbn-sign code directly here.

--- rollup.wbn.js
+++ rollup.wbn.js
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild";
 import * as wbnSign from "wbn-sign";
 import * as rollup from "rollup";
+import base32Encode from "base32-encode";
 import * as fs from "node:fs";
 import * as path from "node:path";
 import * as util from "node:util";
@@ -28,8 +29,74 @@ await esbuild.build({
 
 const { default: wbnOutputPlugin } = await import("./wbn-bundle.js");
 
+// wbn-sign/lib/utils/utils.js
+// A helper function which can be used to parse string formatted keys to
+// KeyObjects.
+function parsePemKey(unparsedKey, passphrase) {
+  return crypto.createPrivateKey({
+    key: unparsedKey,
+    passphrase,
+  });
+}
+function getRawPublicKey(publicKey) {
+  // Currently this is the only way for us to get the raw 32 bytes of the public key.
+  return new Uint8Array(
+    publicKey.export({ type: "spki", format: "der" }).slice(-32),
+  );
+}
+// Throws an error if the key is not a valid Ed25519 key of the specified type.
+function checkIsValidEd25519Key(expectedKeyType, key) {
+  if (key.type !== expectedKeyType) {
+    throw new Error(
+      `Expected key type to be ${expectedKeyType}, but it was "${key.type}".`,
+    );
+  }
+  if (key.asymmetricKeyType !== "ed25519") {
+    throw new Error(
+      `Expected asymmetric key type to be "ed25519", but it was "${key.asymmetricKeyType}".`,
+    );
+  }
+}
+// wbn-sign/lib/web-bundle-id.js
+// Web Bundle ID is a base32-encoded (without padding) ed25519 public key
+// transformed to lowercase. More information:
+// https://github.com/WICG/isolated-web-apps/blob/main/Scheme.md#signed-web-bundle-ids
+class WebBundleId {
+  // https://github.com/WICG/isolated-web-apps/blob/main/Scheme.md#suffix
+  appIdSuffix = [0x00, 0x01, 0x02];
+  scheme = "isolated-app://";
+  key;
+  constructor(ed25519key) {
+    if (ed25519key.asymmetricKeyType !== "ed25519") {
+      throw new Error(
+        `WebBundleId: Only ed25519 keys are currently supported. Your key's type is ${ed25519key.asymmetricKeyType}.`,
+      );
+    }
+    if (ed25519key.type === "private") {
+      this.key = crypto.createPublicKey(ed25519key);
+    } else {
+      this.key = ed25519key;
+    }
+  }
+  serialize() {
+    return base32Encode(
+      new Uint8Array([...getRawPublicKey(this.key), ...this.appIdSuffix]),
+      "RFC4648",
+      { padding: false },
+    ).toLowerCase();
+  }
+  serializeWithIsolatedWebAppOrigin() {
+    return `${this.scheme}${this.serialize()}/`;
+  }
+  toString() {
+    return `\
+  Web Bundle ID: ${this.serialize()}
+  Isolated Web App Origin: ${this.serializeWithIsolatedWebAppOrigin()}`;
+  }
+}
+
 const build = async () => {
-  const key = wbnSign.parsePemKey(
+  const key = parsePemKey(
     privateKey,
   );
   // https://github.com/GoogleChromeLabs/webbundle-plugins/blob/d251f6efbdb41cf8d37b9b7c696fd5c795cdc231/packages/rollup-plugin-webbundle/test/test.js#L408
@@ -50,7 +117,7 @@ const build = async () => {
     input: "./src/script.js",
     plugins: [
       wbnOutputPlugin({
-        baseURL: new wbnSign.WebBundleId(
+        baseURL: new WebBundleId(
           key,
         ).serializeWithIsolatedWebAppOrigin(),
         static: { dir: "assets" },
