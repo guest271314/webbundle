a25805e6bf6348da62c59b6dc3b1d4728ce84409
guest271314 <guest271314@gmail.com>
2023-12-25 19:37:16 -0800

    Include option to use Deno to  install Node.js modules using npm: specifier

---
+++ deno-install-node-modules.js
@@ -0,0 +1,8 @@
+// deno run -A deno-install-node-modules.js
+import "npm:esbuild";
+import "npm:rollup";
+import "npm:wbn";
+import "npm:wbn-sign";
+import "npm:zod";
+import "npm:mime";
+import "npm:base32-encode";
